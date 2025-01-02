c9f4b725318e3f7ee8505742bd5cd5170aee1580
Sasha Ivanova <sashaivanova.pro@gmail.com>
2025-01-02 20:02:46 +0500

    feat: add gh-pages deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss"
 import autoprefixer from "autoprefixer"
 
 export default defineConfig({
+  base: "/react-form-validation",
   plugins: [react()],
   css: {
     postcss: {
